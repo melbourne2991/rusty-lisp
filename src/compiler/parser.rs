38b3d79a46c4f6e6ca29281501def8e2561751ed
Will Leach <wleach1992@gmail.com>
2018-11-12 21:36:28 +1100

    working

--- src/compiler/parser.rs
+++ src/compiler/parser.rs
@@ -1,32 +1,33 @@
 use compiler::lexer::{TokenMetadata, TokenType};
 
+#[derive(Clone, Copy)]
 enum NonTerminal {
   Program,
   List,
 }
 
-enum ASTNodeType<'a> {
+enum ASTNodeType {
   Terminal((TokenType, TokenMetadata)),
-  NonTerminal(ASTNonTerminal<'a>),
+  NonTerminal(ASTNonTerminal),
 }
 
-struct ASTNonTerminal<'a> {
+struct ASTNonTerminal {
   node_type: NonTerminal,
-  children: Vec<&'a ASTNodeType<'a>>,
+  children: Vec<ASTNodeType>,
 }
 
-struct ASTTree<'a> {
-  nodes: Vec<ASTNonTerminal<'a>>,
+struct ASTTree {
+  nodes: Vec<ASTNonTerminal>,
 }
 
-pub struct Parser<'a> {
-  tree: ASTTree<'a>,
+pub struct Parser {
+  tree: ASTTree,
   token: Option<(TokenType, TokenMetadata)>,
-  state: Vec<(NonTerminal, &'a ASTNonTerminal<'a>)>,
+  state: Vec<(NonTerminal, ASTNonTerminal)>,
 }
 
-impl<'a> ASTNonTerminal<'a> {
-  pub fn new(node_type: NonTerminal) -> ASTNonTerminal<'a> {
+impl ASTNonTerminal {
+  pub fn new(node_type: NonTerminal) -> ASTNonTerminal {
     ASTNonTerminal {
       node_type: node_type,
       children: vec![],
@@ -34,8 +35,8 @@ impl<'a> ASTNonTerminal<'a> {
   }
 }
 
-impl<'a> Parser<'a> {
-  pub fn new() -> Parser<'a> {
+impl Parser {
+  pub fn new() -> Parser {
     let parser = Parser {
       tree: ASTTree { nodes: vec![] },
       token: None,
@@ -46,33 +47,37 @@ impl<'a> Parser<'a> {
   }
 
   pub fn feed(&mut self, token: (TokenType, TokenMetadata)) {
-    let current_state = self.state.first();
-
-    match current_state {
-      Program => self.handle_default_state(token),
-      List => {}
-      _ => {}
-    };
+    if let Some(current_state) = self.current_state() {
+      match current_state {
+        NonTerminal::Program => self.handle_default_state(token),
+        NonTerminal::List => {}
+        _ => {}
+      };
+    }
   }
 
   fn new_parser_state(&mut self, node_type: NonTerminal) {
     let node = ASTNonTerminal::new(node_type);
   }
 
-  // fn current_state(&self) -> Option<&NonTerminal> {
-  //   let state = &self.state;
+  fn current_state(&self) -> Option<NonTerminal> {
+    if let Some(&(non_terminal, _)) = self.state.first() {
+      return Some(non_terminal);
+    }
 
-  //   if let Some((non_terminal, _ast)) = state.first() {
-  //     return Some(non_terminal)
-  //   }
-
-  //   None
-  // }
+    None
+  }
 
   fn handle_default_state(&mut self, (token_type, metadata): (TokenType, TokenMetadata)) {
     match token_type {
       TokenType::Whitespace => {}
-      TokenType::LeftParen => {}
+      TokenType::LeftParen => self.state.push((
+        NonTerminal::List,
+        ASTNonTerminal {
+          node_type: NonTerminal::List,
+          children: vec![],
+        },
+      )),
       _ => panic!("Invalid token"),
     }
   }
